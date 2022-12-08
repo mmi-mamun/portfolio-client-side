63972a3254b6f9ce08086fce043e15c7bcd92766
mmi-mamun <muzahidulislam735@gmail.com>
2022-12-08 15:04:51 +0600

    About section added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import cover from '../../src/assets/personal photos/cover.png'
 
 const About = () => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}>
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${cover})` }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className="max-w-md mt-24">
+                    <h1 className="text-5xl font-bold text-purple-300">Hi, I'm Mamun</h1>
+                    <h3 className="text-3xl text-orange-300 font-semibold my-3">I build web apps!</h3>
+                    <p className="py-2 mb-5 text-xl text-black font-bold">I'm a developer based in Dhaka, Bangladesh</p>
                 </div>
             </div>
         </div>
