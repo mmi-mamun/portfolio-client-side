be40e92948adfabdfc6d4fe26ea09b29b5c76e16
mmi-mamun <muzahidulislam735@gmail.com>
2022-12-11 23:24:45 +0600

    pdf file of resume updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import cover from '../../src/assets/personal photos/cover.png'
+import cover from '../../src/assets/logo/logo-2.png'
 
 const About = () => {
     return (
         <div className="hero min-h-screen" style={{ backgroundImage: `url(${cover})` }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md mt-24">
+                <div className="mt-24">
                     <h1 className="text-5xl font-bold text-purple-300">Hi, I'm Mamun</h1>
-                    <h3 className="text-3xl text-orange-300 font-semibold my-3">I build web apps!</h3>
-                    <p className="py-2 mb-5 text-xl text-black font-bold">I'm a developer based in Dhaka, Bangladesh</p>
+                    <h3 className="px-12 lg:text-xl text-black font-bold my-3 text-justify">I'm from Dhaka, Bangladesh. I've completed my Bachelor's degree from Daffodil International University in the Department of Computer Science & Engineering.</h3>
+                    <p className="px-12 py-2 mb-5 lg:text-xl text-black font-bold">I love programming, developing new products and exploring new technologies. Also, I love to play video games and hang out with friends in my free time.</p>
+
+                    <p className="my-12 text-2xl font-semibold">*Thank you so much for visiting my profile*</p>
                 </div>
             </div>
         </div>
